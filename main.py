cc8e6c0ed35d0c965bb06fabe7db5ee919a49081
Saendhor <comix143.mariorossi@gmail.com>
2022-05-26 12:32:08 +0200

    feat: function now replies to messages with '?' and filters bot messages along with commands; [NEEDS FURTHER TESTING]

--- main.py
+++ main.py
@@ -19,13 +19,21 @@ import random
 #check whether this works just fine here or you got to move it lower the start function
 updater.start_polling()
 
-#handler that reads normal messages and sends a text when
+#this function is capable of reading the messages in the chat and subsequently answer them recalling the function "echo()"
+#NOTE: it only identifies messages which:
+#do not come from the bot;
+#are not commands;
+#have in their string the "?" character.
+
+autoresponse_text = echo()
+
 def autoresponse(update: Update, context: CallbackContext):
     context.bot.send_message(chat_id = update.effective_chat.id,
+    reply_to_message_id = message.message_id,
     text = autoresponse_text
     )
 
-autoresponse_handler = MessageHandler(Filters.text & (~Filters.command | ~Filters.video | ~Filters.animation | ~Filters.audio | ~ Filters.document), autoresponse)
+autoresponse_handler = MessageHandler(Filters.regex(r".*\?+.*") & ~Filters.command & ~Filters.user(707448824), autoresponse)
 dispatcher.add_handler(autoresponse_handler)
 
 #http://python-telegram-bot.readthedocs.io/en/latest/telegram.ext.messagehandler.html
